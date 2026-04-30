9f0ac2448634820b3a57e8533b1793b20b991ecd
OverYander <gavinchen8706@gmail.com>
2026-04-29 22:32:41 -0400

    new route for analysis w/ file

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template,redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files.get("file")
+    if not file or file.filename == "":
+        return render_template("index.html")
+    file_data = file.read()  # file is in memory only rn
+    return render_template("result.html", filename=file.filename)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
